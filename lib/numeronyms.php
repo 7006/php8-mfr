57e3e8f8fc3f04e2ac464eaff032e7bc2fcd54c1
7006 <22399553+7006@users.noreply.github.com>
2024-09-30 17:06:48 +0300

    Update numeronyms.php

--- lib/numeronyms.php
+++ lib/numeronyms.php
@@ -1,16 +1,14 @@
 <?php
 
-define('NUMERONYM_LENGTH', 4);
 define('INPUT', 'Every developer likes to mix kubernetes and javascript');
 
 $words = preg_split('/\s+/', INPUT, -1, PREG_SPLIT_NO_EMPTY);
 
 function numeronym($word)
 {
-    if (strlen($word) < NUMERONYM_LENGTH) {
-        return $word;
-    }
-    return $word[0] . ($len - 2) . $word[$len - 1];
+    return strlen($word) < 4
+        ? $word
+        : $word[0] . ($len - 2) . $word[$len - 1];
 }
 
 $numeronyms = array_map(numeronym(...), $words);
